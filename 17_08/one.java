4663b4b6e1f40e3c0259f8845f2340a2c196aebb
rahulpatel2727 <rahul23131420@gmail.com>
2022-08-17 14:31:43 +0530

    new

--- 17_08/one.java
+++ 17_08/one.java
@@ -2,9 +2,18 @@ import java.util.Scanner;
 
 public class one{
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        int n=s.nextInt();
-        
+        // Scanner s = new Scanner(System.in);
+        int n=5;
+        int i=1;
+        while(i<=n){
+            int j=1;
+            while(j<=i){
+                System.out.print("*"+" ");
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
         
     }
 }
