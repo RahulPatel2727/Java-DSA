7bfe27050f6b7aa533de07e4fa3080f4eed995d3
rahulpatel2727 <rahul23131420@gmail.com>
2022-08-17 14:32:08 +0530

    new

--- 17_08/one.java
+++ 17_08/one.java
@@ -7,6 +7,7 @@ public class one{
         int i=1;
         while(i<=n){
             int j=1;
+            
             while(j<=i){
                 System.out.print("*"+" ");
                 j++;
