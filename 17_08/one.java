421d977ff16e80c05ab5aaf8ee9adc5939009aa2
rahulpatel2727 <rahul23131420@gmail.com>
2022-08-19 13:13:19 +0530

    new

--- 17_08/one.java
+++ 17_08/one.java
@@ -1,11 +1,20 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class one{
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        int n = s.nextInt();
-        int ii=0;
+        List<Integer> list=new ArrayList<>();
+        list.add(1);
+        list.add(0);
+        list.add(31);
+        list.add(14);
+        list.add(18);
+        list.add(10);
+        System.out.println(list);
+        Collections.sort(list);
+        System.out.println(list);
         
+
+
         
     }
 }
