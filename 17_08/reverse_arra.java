3b59f4a92fad27d1ef22a187b3be43a7883e93f9
rahulpatel2727 <rahul23131420@gmail.com>
2022-08-25 15:15:11 +0530

    new

---
+++ 17_08/reverse_arra.java
@@ -0,0 +1,14 @@
+public class reverse_arra {
+    public static void main(String[] args) {
+        int []arr={2,5,8,1,2,3,9,5};
+        int i=0,j=arr.length-1;
+        while(i<j) {
+            int temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;i++;j--;
+        }
+        for(int ii:arr)
+            System.out.print(ii+" ");
+    }
+    
+}
