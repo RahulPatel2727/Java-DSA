957f4398b26c5ea4f02b2ee1d2439d608a544525
rahulpatel2727 <rahul23131420@gmail.com>
2022-09-05 19:33:12 +0530

    new

--- recursion.java
+++ recursion.java
@@ -1,9 +1,12 @@
 public class recursion {
     static int i=1;
     public static void main(String[] args) {
-        name(8);
+        name(8,1);
     }
-    private static void name(int n){
-        
+    private static void name(int n,int i){
+        if(i>n)
+            return;
+        name(n,i+1);
+        System.out.print(i+" ");
     }
 }
